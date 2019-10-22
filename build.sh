4622ea4021ca6086fec41acbbe8578df459bd262
Elliot Barlas <elliotbarlas@gmail.com>
2019-10-22 14:07:09 -0700

    use matplotlib linux wheel for lambda zip build.

--- build.sh
+++ build.sh
@@ -9,8 +9,7 @@ cd target/
 pip install requests -t ./
 pip install pytz -t ./
 pip install python-twitter -t ./
-pip install numpy -t ./
-pip install matplotlib -t ./
+pip install https://files.pythonhosted.org/packages/19/7a/60bd79c5d79559150f8bba866dd7d434f0a170312e4d15e8aefa5faba294/matplotlib-3.1.1-cp37-cp37m-manylinux1_x86_64.whl -t ./
 zip -r buoy-lambda.zip *
 
 cd -
